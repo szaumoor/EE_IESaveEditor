703e73f967dfc365f69f9356fa81783ead8466ca
szaumoor <szaumoor@tuta.io>
2025-11-30 23:24:08 +0100

    Some test utils and small refactorings

--- tests/gam_file_tests.cpp
+++ tests/gam_file_tests.cpp
@@ -6,76 +6,48 @@
 #include "../src/backend/ie_files.h"
 #include "../src/backend/gam_file.h"
 
-using namespace std;
-
-// class GamFileTests : public ::testing::Test {
-// protected:
-//     const char* path = "invalid_version.gam";
-//
-//
-//     void TearDown() override {
-//         std::error_code ec;
-//         std::filesystem::remove(filesystem::path(path), ec);
-//     }
-// };
+#include "utils/tests_helper.h"
 
 constexpr auto real_gam = "../tests/res/BALDUR.gam";
 
-auto gam_with(const char* name, const char* signature, const char* version) -> void {
-    ofstream ofs( name, ios::binary );
-    ofs.write( signature, 4 );
-    ofs.write( version, 4 );
-    ofs.close();
-}
 
 TEST( GamFileTests, GamIsUnreadableTest )
 {
-    EXPECT_TRUE( GamFile("nonexistent.gam").unreadable() );
+    ASSERT_TRUE( GamFile("nonexistent.gam").unreadable() );
 }
 
 TEST( GamFileTests, GamIsMalformedVersion )
 {
-    const auto path = "invalid_version.gam";
-    gam_with(path, "GAME", "Invl");
-
-    EXPECT_TRUE( GamFile(path).malformed() );
-    filesystem::remove( filesystem::path(path) );
+    const auto temp = TempCreator("invalid_version.gam", "GAME", "Invl");
+    ASSERT_TRUE( GamFile(temp.name).malformed() );
 }
 
 TEST( GamFileTests, RealGamIsReadableAndValid )
 {
-    EXPECT_TRUE( GamFile( real_gam ).good() );
+    ASSERT_TRUE( GamFile( real_gam ).good() );
 }
 
 TEST( GamFileTests, GamIsReadableAndValid )
 {
-    const auto path = "valid_version.gam";
-    gam_with(path, "GAME", "V2.0");
-
-    EXPECT_TRUE( GamFile( path ).good() );
-    filesystem::remove( path );
+    const auto temp = TempCreator("valid_version.gam", "GAME", "V2.0");
+    ASSERT_TRUE( GamFile( temp.name ).good() );
 }
 
 TEST( GamFileTests, GamIsReadableAndValidTwoPointOne )
 {
-    const auto path = "valid_version.gam";
-    gam_with(path, "GAME", "V2.1");
-
-    EXPECT_TRUE( GamFile( path ).good() );
-    filesystem::remove( path );
+    const auto temp = TempCreator("valid_version.gam", "GAME", "V2.1");
+    ASSERT_TRUE( GamFile( temp.name ).good() );
 }
 
 TEST( GamFileTests, GamIsMalformedSignature )
 {
-    const auto path = "invalid_signature.gam";
-    gam_with(path, "XXXX", "V2.0");
-    EXPECT_TRUE( GamFile( path ).malformed() );
-    filesystem::remove( path );
+    const auto temp = TempCreator("invalid_signature.gam", "XXXX", "V2.0");
+    ASSERT_TRUE( GamFile( temp.name ).malformed() );
 }
 
 TEST( GamFileTests, GamHasAtLeastOnePartyMember )
 {
     const GamFile gam( real_gam );
-    EXPECT_TRUE( gam.good() );
-    EXPECT_GT(gam._party_members.size(),1);
+    ASSERT_TRUE( gam.good() );
+    ASSERT_GT(gam._party_members.size(),1);
 }
--- tests/tlk_file_tests.cpp
+++ tests/tlk_file_tests.cpp
@@ -7,84 +7,66 @@
 #include "../src/backend/tlk_file.h"
 #include "../src/backend/ie_files.h"
 
-
-using namespace std;
+#include "utils/tests_helper.h"
 
 constexpr auto real_path = "../tests/res/dialog.tlk";
 
 TEST( TlkFileTest, TlkIsUnreadableTest )
 {
-    EXPECT_TRUE( TlkFile( "nonexistent.tlk" ).unreadable() );
+    ASSERT_TRUE( TlkFile( "nonexistent.tlk" ).unreadable() );
 }
 
 TEST( TlkFileTest, TlkStringCountZeroIfInvalid )
 {
     const auto tlk = TlkFile( "nonexistent.tlk" );
-    EXPECT_TRUE( tlk.unreadable() && tlk.length() == 0 );
+    ASSERT_TRUE( tlk.unreadable() && tlk.length() == 0 );
 }
 
 TEST( TlkFileTest, TlkIsMalformedVersion )
 {
-    ofstream ofs( "invalid_version.tlk", ios::binary);
-    ofs.write("TLK ", 4); // Valid signature
-    ofs.write( "Invl", 4 ); // Invalid version
-    ofs.close();
-    EXPECT_TRUE( TlkFile( "invalid_version.tlk" ).malformed() );
-    filesystem::remove( "invalid_version.tlk" );
+    const auto temp = TempCreator( "invalid_version.tlk", "TLK ", "Invl" );
+    ASSERT_TRUE( TlkFile( temp.name ).malformed() );
 }
 
 TEST( TlkFileTest, TlkIsMalformedSignature )
 {
-    ofstream ofs( "invalid_signature.tlk", ios::binary );
-    ofs.write( "XXXX", 4 ); // Invalid signature
-    ofs.write( "V1  ", 4 ); // Valid version
-    ofs.close();
-    EXPECT_TRUE( TlkFile( "invalid_signature.tlk" ).malformed() );
-    filesystem::remove( "invalid_signature.tlk" );
+    const auto temp = TempCreator( "invalid_signature.tlk", "XXXX", "V1  " );
+    ASSERT_TRUE( TlkFile( temp.name ).malformed() );
 }
 
 TEST( TlkFileTest, TlkIsReadableAndValid )
 {
-    ofstream ofs( "valid_tlk.tlk", ios::binary );
-    ofs.write( "TLK ", 4 ); // Valid signature
-    ofs.write( "V1  ", 4 ); // Valid version
-    ofs.close();
-    EXPECT_TRUE( TlkFile( "valid_tlk.tlk" ).good() );
-    filesystem::remove( "valid_tlk.tlk" );
+    const auto temp = TempCreator( "valid_tlk.tlk", "TLK ", "V1  " );
+    ASSERT_TRUE( TlkFile( temp.name ).good() );
 }
 
 TEST( TlkFileTest, TlkHasExpectedTextAtIndexOne )
 {
     const TlkFile tlk( real_path );
-    EXPECT_TRUE( tlk.good() );
+    ASSERT_TRUE( tlk.good() );
+
     const auto result = tlk.at_index( 1 );
-    EXPECT_TRUE( result.has_value() );
-    EXPECT_TRUE( *result == "No, I'm sorry, none of them sound familiar." );
+    ASSERT_TRUE( result.has_value() );
+    ASSERT_TRUE( *result == "No, I'm sorry, none of them sound familiar." );
 }
 
 TEST( TlkFileTest, TlkHasCantAccessInvalidIndexes)
 {
     const TlkFile tlk( real_path );
-    EXPECT_TRUE( tlk.good() );
+    ASSERT_TRUE( tlk.good() );
+
     const auto result1 = tlk.at_index( -1 );
     const auto result2 = tlk.at_index( tlk.length() );
-    EXPECT_TRUE( !result1.has_value() && !result2.has_value() );
+    ASSERT_TRUE( !result1.has_value() && !result2.has_value() );
 }
 
 TEST( TlkFileTest, TlkReturnsUnexpectedIfFileIsMalformed )
 {
-    ofstream ofs0( "invalid_signature0.tlk", ios::binary );
-    ofs0.write( "XXXX", 4 ); // Invalid signature
-    ofs0.write( "V1  ", 4 ); // Valid version
-    ofs0.close();
-    ofstream ofs1( "invalid_signature1.tlk", ios::binary );
-    ofs1.write( "TLK ", 4 ); // Invalid signature
-    ofs1.write( "Invl", 4 ); // Valid version
-    ofs1.close();
-    const auto tlk0 = TlkFile( "invalid_signature0.tlk");
-    EXPECT_FALSE( tlk0.at_index(1).has_value() );
-    const auto tlk1 = TlkFile( "invalid_signature1.tlk");
-    EXPECT_FALSE( tlk1.at_index(1).has_value() );
-    filesystem::remove( "invalid_signature0.tlk" );
-    filesystem::remove( "invalid_signature1.tlk" );
+    const auto temp0 = TempCreator( "invalid_version.tlk", "XXXX", "V1  " );
+    const auto tlk0 = TlkFile( temp0.name);
+    EXPECT_FALSE(tlk0.at_index(1).has_value());
+
+    const auto temp1 = TempCreator("invalid_signature1.tlk", "TLK ", "Invl");
+    const auto tlk1 = TlkFile( temp1.name);
+    ASSERT_FALSE( tlk1.at_index(1).has_value() );
 }
