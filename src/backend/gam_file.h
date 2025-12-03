4bbc0530783d31346bc494b8c4d3f5b881abda66
szaumoor <szaumoor@tuta.io>
2025-12-03 23:19:19 +0100

    Refactor

--- src/backend/gam_file.h
+++ src/backend/gam_file.h
@@ -1,6 +1,9 @@
 #ifndef GAM_FILE_H
 #define GAM_FILE_H
 
+#include <expected>
+
+#include "binary_layouts/gam.h"
 #include "cre_file.h"
 #include "utils/helper_structs.h"
 #include "ie_files.h"
@@ -8,22 +11,20 @@
 #include <string_view>
 #include <vector>
 
-#include "binary_layouts/gam.h"
+class GamFile;
 
+using PossibleGamFile = std::expected<GamFile, IEError>;
 
 class GamFile final : public IEFile
 {
 public:
+    static PossibleGamFile open(std::string_view path) noexcept;
+private:
     explicit GamFile( std::string_view path ) noexcept;
-
-    GamFile() = delete;
+    GamHeader _header{};
 
     std::vector<GamCharacterData> _party_members;
     std::vector<CreFile> _party_cre_files;
-
-private:
-    GamHeader _header;
-
     std::vector<GamCharacterData> _non_party_members;
     std::vector<CreFile> _non_party_cre_files;
     std::vector<GamGlobalVariable> _variables;
@@ -31,7 +32,7 @@ private:
     std::vector<GamLocationInfo> _stored_locations;
     std::vector<GamLocationInfo> _pocket_plane_info;
 
-    GamFamiliarInfo _familiar_info;
+    GamFamiliarInfo _familiar_info{};
     std::vector<Resref> _familiar_extras;
 
     void check_for_malformation() noexcept override;
