bae89087ce0f8470751296e75ba121e24e224516
szaumoor <szaumoor@tuta.io>
2026-09-16 13:33:08 +0200

    chore: base class made non instantiable
    Signed-off-by: szaumoor <szaumoor@tuta.io>

--- src/backend/utils/ie_class.hpp
+++ src/backend/utils/ie_class.hpp
@@ -5,6 +5,9 @@
  * Empty struct to define a no-storage cost, zero-overhead interface for all other IE-related
  * classes. This exists purely as a convenience to tag domain types.
  */
-struct IEClass {};
+struct IEClass {
+protected:
+    IEClass() = default;
+};
 
 #endif //EE_SAVEEDITOR_IE_CLASS_H
